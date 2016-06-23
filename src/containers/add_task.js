93b79cf37d5f8e3d3fc1f1cc2050240560d66df4
gurumohanreddy <gurumohan449@gmail.com>
2016-06-23 06:59:13 +0530

    submit button basic version

--- src/containers/add_task.js
+++ src/containers/add_task.js
@@ -1,12 +1,36 @@
 import React,{ Component } from 'react';
 
 export default class AddTask extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={term:''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onsubmit = this.onsubmit.bind(this);
+
+  }
+
+  onInputChange(event){
+    this.setState({term:event.target.value});
+  }
+
+  onsubmit(){
+    
+    console.log(this.state.term);
+  }
+
+
   render(){
     return(
       <div className='input-group'>
-      <input />
+      <input
+      placeholder='Add a task'
+      value={this.state.term}
+      className='form-control'
+      onChange={this.onInputChange}/>
       <span className='input-group btn'>
-        <button type='submit' className='btn btn-secondary'>Submit</button>
+        <button type='submit' className='btn btn-secondary' onClick={this.onsubmit}>Submit</button>
       </span>
       </div>
     )
